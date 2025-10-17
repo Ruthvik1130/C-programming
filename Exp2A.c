ded01e8997bbe83584623f6021e2359694c2d92a
Ruthvik1130 <ruthvik22318khs2021@gmail.com>
2025-10-17 16:01:24 +0530

    Add files via upload

---
+++ Exp2A.c
@@ -0,0 +1,13 @@
+//Ruthvik Kundar Uin:-251P130 Computer Engineering D3
+#include <stdio.h>
+
+int main() {
+int num,mod;
+
+printf("Enter a number: ");
+scanf("%d",&num);
+
+mod = num%2;
+
+(mod>0? printf("The given number is odd"): printf("The given number is even"));
+return 0;}
