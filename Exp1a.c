21b5f08f85715794fff5fca026790a50b9276cf6
Ruthvik1130 <ruthvik22318khs2021@gmail.com>
2025-11-03 09:41:22 +0530

    Update Exp1a.c

--- Exp1a.c
+++ Exp1a.c
@@ -1,6 +1,7 @@
 // Name:Ruthvik Kundar UIN:251P130 Computer Engineering D3 
 #include <stdio.h>
 int main(){
-    printf("Name :-Sahil Shaikh \nAge:-18 \nClass :-F.E, \ndiv:- D \nUIN:-251P136\n");
+    printf("Name :-Ruthvik kundar \nAge:-17 \nClass :-F.E, \ndiv:- D \nUIN:-251P130\n");
     return 0;
 }
+
